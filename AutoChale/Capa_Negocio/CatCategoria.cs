9f79929e63ca870ec46fd36212fcba26781cd00b
unknown <mendoza25michael@gmail.com>
2022-05-24 08:50:19 -0600

    Mi primer Comit

---
+++ AutoChale/Capa_Negocio/CatCategoria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CAPA_DATOS;
+
+namespace Capa_Negocio
+{
+    public class CatCategoria
+    {
+        public string TableName = "Categoria";
+        public int CodigoCatcategoria { get; set; }
+        char estado;
+        int codigo;
+        
+        public object Save(CatCategoria inst)
+        {
+            try
+            {
+                SqlADOConexion.IniciarConexion("Raiko", "Akatsukinar0t0");
+                {
+                    if (inst.CodigoCatcategoria== -1)
+                    {
+                        return SqlADOConexion.SQLM.InsertObject(TableName, inst);
+
+                    }
+                    else
+                    {
+                        return SqlADOConexion.SQLM.UpdateObject(TableName, inst, "CodigoCategoria");
+                    }
+                }
+                catch(Exception ex);
+            }
+        }
+        throw ;
+    }
+
+}
